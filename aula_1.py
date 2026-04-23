a3d0d728f6a058492eaabc0d2f65f69dcbc1cf2c
Jimi-45 <jcdvivo2015l3@gmail.com>
2026-04-23 16:14:40 -0300

    Primeiro commit

---
+++ aula_1.py
@@ -0,0 +1,84 @@
+import subprocess
+
+restaurantes = ['CanelaPau', 'California Bay']
+
+def exibir_nome_do_programa():
+    print('''
+░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
+██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
+╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
+░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
+██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
+╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░
+      ''')
+
+
+def mostrar_opcoes():
+    print('1. Cadastrar restaurante')
+    print('2. Listar restaurante')
+    print('3. Ativar restaurante')
+    print('4. Sair\n')
+
+
+def finalizar_app():
+    subprocess.run('cls', shell=True)
+    print('Finalizando Programa')
+
+def voltar_ao_menu_principal():
+    input('\nDigite Enter para voltar ao menu principal')
+    main()
+
+def opcao_invalida():
+    print('Opção inválida!\n')
+    voltar_ao_menu_principal()
+
+def exibir_subtitulo(texto):
+    subprocess.run('cls', shell=True)
+    print(texto)
+    print()
+
+def cadastrar_novo_restaurante():
+    exibir_subtitulo('Cadastro de restaurantes')
+    cadastro = input('Digite um nome de um restaurante que deseja cadastrar: \n')
+    restaurantes.append(cadastro)
+    print(f'O restaurante {cadastro} foi cadastrado com sucesso!!')
+    voltar_ao_menu_principal()
+
+
+def listar_restaurantes():
+    exibir_subtitulo('Lista de restaurantes')
+
+    for restaurante in restaurantes:
+        print('.',restaurante)
+
+    voltar_ao_menu_principal()
+
+
+def escolher_opcao():
+    try:
+        opcao_escolhida = int(input('Escolha uma opção: '))
+
+        if opcao_escolhida == 1:
+            cadastrar_novo_restaurante()
+        elif opcao_escolhida == 2:
+            listar_restaurantes()
+        elif opcao_escolhida == 3:
+            print('Ativar restaurante')
+        elif opcao_escolhida == 4:
+            finalizar_app()
+        else:
+            opcao_invalida()
+    except:
+        opcao_invalida()
+
+
+def main():
+    subprocess.run('cls', shell=True)
+    exibir_nome_do_programa()
+    mostrar_opcoes()
+    escolher_opcao()
+   
+
+     
+if __name__ == '__main__':
+        main()
